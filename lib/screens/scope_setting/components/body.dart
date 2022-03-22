0f671b8ddb8318edfc9129083a80efee2a966d8d
soyeon <no02031@naver.com>
2022-03-22 16:31:34 +0900

    setting scope

--- lib/screens/scope_setting/components/body.dart
+++ lib/screens/scope_setting/components/body.dart
@@ -1,54 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/setting/setting_screen.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../../../size_config.dart';
-import '../../../constants.dart';
+import 'package:shop_app/screens/setting/setting_screen.dart';
+
+import 'scope_setting_form.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      //padding: EdgeInsets.symmetric(vertical: 1),
-      child: Column(
-        children: [
-          Row(
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                SizedBox(width: getProportionateScreenWidth(12),),
-                IconButton(
-                    icon: SvgPicture.asset("assets/icons/Back ICon.svg"),
-                    onPressed: () {
-                      Navigator.pushNamed(context, SettingScreen.routeName);
-                    }
-                ),
-                SizedBox(width: getProportionateScreenWidth(80),),
-                Container(
-                  alignment: Alignment(-0.8, 0.0),
-                  child: Text(
-                    "소개 범위 설정",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: getProportionateScreenWidth(15),
+                Row(
+                  children: [
+                    //SizedBox(width: getProportionateScreenWidth(5),),
+                    IconButton(
+                        icon: SvgPicture.asset("assets/icons/Back ICon.svg"),
+                        onPressed: () {
+                          Navigator.pushNamed(context, SettingScreen.routeName);
+                        }
                     ),
-                  ),
-                ),
-                SizedBox(width: getProportionateScreenWidth(60),),
-                TextButton(
-                  child: Text(
-                    "저장",
-                    style: TextStyle(
-                      color: kPrimaryColor,
-                      fontSize: getProportionateScreenWidth(18),
-                      //fontWeight: FontWeight.bold,
+                    SizedBox(width: getProportionateScreenWidth(65),),
+                    Container(
+                      alignment: Alignment(-0.8, 0.0),
+                      child: Text(
+                        "소개 범위 설정",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: getProportionateScreenWidth(15),
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, SettingScreen.routeName);
-                  },
+                    SizedBox(width: getProportionateScreenWidth(50),),
+                    TextButton(
+                      child: Text(
+                        "저장",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontSize: getProportionateScreenWidth(18),
+                          //fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, SettingScreen.routeName);
+                      },
+                    ),
+                  ],
                 ),
-              ]
+                SizedBox(height: getProportionateScreenHeight(30)),
+                ScopeSettingForm(),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
